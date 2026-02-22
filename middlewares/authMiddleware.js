03adc9356b64f55852c43e795903d0c15a6487f6
Victor Devolper <vixtoru8@gmail.com>
2026-02-22 11:49:32 -0300

    Fiz para tentar encontrar o towen, comecei agora middleware de validação

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,20 @@
 import jwt from "jsonwebtoken";
+
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization?.split()[1] || req.cookies.token; // pega o token de dentro da req no header o ? é caso não tenha nada manda nada o outro procura nos cookies
+    // Se o token não for encontrado
+    if (!token) {
+        res.status(401).json({ error: "token não fornecido!" })
+    }
+    // Agora um try catch
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch {
+        return res.status(401).json({ error: "Token invalido!" })
+    }
+
+}
