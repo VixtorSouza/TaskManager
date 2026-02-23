3712985165d741f09831fbf26032ecd04e405339
Victor Devolper <vixtoru8@gmail.com>
2026-02-23 14:05:58 -0300

    Criei a parte do login, tenho que pegar email, senha, verificar se existe o usuario, se a senha é valida e bate com a do Banco de Dados, caso não cria um token, passa o secret e o tempo de expiração que normalmente deve ser baixa de 15m a 1h, em seguida crio um cookie onde armazeno esse token com httOnly true para não vazar no Front, maximo de de tempo do cookie e secure que é para HTTPS e deve ser ativado nas proximas etapas. Agora irei iniciar o controller de User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 import { userModel } from "../model/userModel";
 import bcrypt from "bcrypt";
 import { v7 as uuidv7 } from "uuid";
-
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 export class authController {
     static async register(req, res, next) {
@@ -38,4 +39,48 @@ export class authController {
 
 
     }
+    static async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+            if (!email || !password) {
+                res.status(400).json({ error: "Todos os campos são obrigatórios" })
+                return;
+            }
+            const user = userModel.FindUserByEmail({ email });
+            if (!user) {
+                res.status(404).json({ error: "Usuario nao encontrado" })
+                return;
+            }
+            const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+            if (!isPasswordValid) {
+                res.status(401).json({ error: "Senha invalida" })
+                return;
+            }
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            // Em vez de apenas res.json, você "seta" o cookie antes
+            res.cookie('token', token, {
+                httpOnly: true, // O front-end não consegue ler/roubar via script
+                secure: false,  // Coloque 'true' quando tiver HTTPS (produção), preciso lembrar
+                maxAge: 3600000 // 1 hora em milissegundos
+            });
+            res.status(200).json({ message: "Sucess", user: { name, email, id: user.id } })
+            return token;
+
+        } catch (err) {
+            next(err)
+        }
+
+    }
+    static async logout(req, res, next) {
+        // o que faz 
+
+        try {
+            res.clearCookie('token'); // limpa o cookie pelo nome dele que dentro tem um token que foi criado no login
+            res.status(200).json({ message: "Sucess" })
+        } catch (err) {
+            next(err)
+        }
+
+    }
+
 }
