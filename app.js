0517d5d6bcc2c449be9b036b9bde42301415a76f
Victor Devolper <vixtoru8@gmail.com>
2026-02-24 10:57:50 -0300

    Completei meu app.js, adicionei dotenv para conseguir usar meu .env, adicionei as rotas faltando e comentei, além de adicionar error handle no final para assegurar que pegue os erros e também implementei auth

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+import 'dotenv/config'
 import express from "express";
 import userRoutes from "./routes/userRoutes.js"; // gerenciar usuarios, CRUD completo
-import taskRoutes from "./routes/taskRoutes.js"; // gerenciar tarefas
-import projectRoutes from "./routes/projectRoutes.js"; // gerenciar projetos
+//import taskRoutes from "./routes/taskRoutes.js"; // gerenciar tarefas
+//import projectRoutes from "./routes/projectRoutes.js"; // gerenciar projetos
+import { authRoutes } from "./routes/authRoutes.js"
+import { errorHandler } from "./middlewares/errorHandles.js"
+
 
 const app = express()
 const PORT = process.env.PORT ?? 3000; // essencial para rodar em qualquer lugar
 
 // cuidar para deixar em formato json e enviar para as rotas
 app.use(express.json());
+app.use("/auth", authRoutes)
 app.use("/users", userRoutes);
-app.use("/tasks", taskRoutes);
-app.use("/projects", projectRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+// DEPOIS de todas as rotas:
+app.use(errorHandler);  // ← sempre por último!
+// pega os erros nas rotas 
